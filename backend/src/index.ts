dc1decd3cf4853ef0785c871a3c5260efdff8788
jpache22 <133060445+jpache22@users.noreply.github.com>
2025-10-07 10:15:12 -0700

    Set up basic API test
    Not sure if this works but let's check.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,33 @@
 import { Hono } from 'hono'
+import { Client } from "pg";
 
-const app = new Hono()
+type Bindings = {
+  HYPERDRIVE: Hyperdrive,
+}
+
+const app = new Hono<{Bindings: Bindings}>()
+
+
 
 app.get('/', (c) => {
   return c.text('Hello Cloudflare Workers!')
+})
+
+// test api call to database
+app.get('/db-test', async (context) => {
+  const connectionString = context.env.HYPERDRIVE.connectionString;
+
+  const client = new Client({connectionString});
+  
+  try {
+    // query the current data and time
+    await client.connect(); // connect to the database
+    const result = await client.query(`SELECT NOW();`); // SQL query
+    return context.json({success: true, date: result.rows[0].now});
+  } catch(err: any) {
+    console.error(err);
+    return context.json({success: false, error: err.message}, 500);
+  }
 })
 
 export default app
