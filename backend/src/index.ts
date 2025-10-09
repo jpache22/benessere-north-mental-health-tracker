8f7340ad10e27fa5abe0fb3f4803650a82240d96
jpache22 <133060445+jpache22@users.noreply.github.com>
2025-10-08 20:44:58 -0700

    Test frontend API call

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,29 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors';
 import { Client } from "pg";
 
 type Bindings = {
   HYPERDRIVE: Hyperdrive,
+  MAIN_PAGES_URL: string,
+  DEV_PAGES_URL: string
 }
 
 const app = new Hono<{Bindings: Bindings}>()
 
+// must add Cross-origin resource sharing permissions for the pages urls
+app.use('*', async(context, next) => { // next is a function that tells hono to continue to the next middleware/route handler
+  const corsMiddleware = cors({
+    origin: [context.env.DEV_PAGES_URL, context.env.MAIN_PAGES_URL],
+    allowMethods: ['GET'],
+    credentials: true
+  });
 
+  return corsMiddleware(context, next);
+});
 
 app.get('/', (c) => {
   return c.text('Hello Cloudflare Workers!')
-})
+});
 
 // test api call to database
 app.get('/db-test', async (context) => {
