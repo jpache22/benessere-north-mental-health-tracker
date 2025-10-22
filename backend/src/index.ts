a15f3d63e94552ca989ad66f6b5ee49da9624f83
jmaser22 <jmaser@asu.edu>
2025-10-21 18:28:51 -0700

    using bcryptjs to fix build error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors';
 import { Client } from "pg";
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import { Pool } from "pg";
